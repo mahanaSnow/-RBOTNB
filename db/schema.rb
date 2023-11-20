3b315cec8be6ab9192da771e143d06cfba7f96cc
Anthony Girard <antho.girard18@gmail.com>
2023-11-20 15:30:56 +0100

    model, controller, schema creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_133229) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_20_140835) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_133229) do
     t.decimal "price_per_day"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_bots_on_user_id"
   end
 
   create_table "rentals", force: :cascade do |t|
@@ -29,8 +31,24 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_133229) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["bot_id"], name: "index_rentals_on_bot_id"
+    t.index ["user_id"], name: "index_rentals_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "bots", "users"
   add_foreign_key "rentals", "bots"
+  add_foreign_key "rentals", "users"
 end
